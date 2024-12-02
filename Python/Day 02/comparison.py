f5a5c09b7d5b5a96a060c2666d94a0c44d523970
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-02 15:50:50 +0530

    Create comparison.py

---
+++ Python/Day 02/comparison.py
@@ -0,0 +1,8 @@
+x, y = 5, 8
+
+print("x == y:", x == y)  # Output: False
+print("x != y:", x != y)  # Output: True
+print("x > y:", x > y)    # Output: False
+print("x < y:", x < y)    # Output: True
+print("x >= y:", x >= y)  # Output: False
+print("x <= y:", x <= y)  # Output: True
