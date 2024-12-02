2afd59a5a5510bc91aad025c0bf702dfd0796982
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-02 15:53:22 +0530

    Create example3.py

---
+++ Python/Day 02/example3.py
@@ -0,0 +1,7 @@
+#Logical Operators to Check Range
+num = 15
+
+if num > 10 and num < 20:
+    print(num, "is between 10 and 20")  # Output: 15 is between 10 and 20
+else:
+    print(num, "is not in range")
