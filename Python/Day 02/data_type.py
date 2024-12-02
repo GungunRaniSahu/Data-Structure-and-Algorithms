48ec24330c9d490518a1f705762b8a812fab9149
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-02 15:48:54 +0530

    Create data_type.py

---
+++ Python/Day 02/data_type.py
@@ -0,0 +1,23 @@
+# String
+my_string = "Hello, Python!"
+print(my_string, type(my_string))  # Output: Hello, Python! <class 'str'>
+
+# Boolean
+my_bool = True
+print(my_bool, type(my_bool))  # Output: True <class 'bool'>
+
+# List
+my_list = [1, 2, 3, "Python"]
+print(my_list, type(my_list))  # Output: [1, 2, 3, 'Python'] <class 'list'>
+
+# Tuple
+my_tuple = (1, 2, 3)
+print(my_tuple, type(my_tuple))  # Output: (1, 2, 3) <class 'tuple'>
+
+# Dictionary
+my_dict = {"name": "Gungun", "age": 21}
+print(my_dict, type(my_dict))  # Output: {'name': 'Gungun', 'age': 21} <class 'dict'>
+
+# Set
+my_set = {1, 2, 3}
+print(my_set, type(my_set))  # Output: {1, 2, 3} <class 'set'>
