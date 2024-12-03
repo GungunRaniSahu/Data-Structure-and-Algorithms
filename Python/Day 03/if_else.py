5d03b1e9f8f0f18cc6def5312d1f5e4bed6e8af4
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-03 14:32:00 +0530

    Create if_else.py

---
+++ Python/Day 03/if_else.py
@@ -0,0 +1,9 @@
+#Check if a Number is Positive, Negative, or Zero
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
