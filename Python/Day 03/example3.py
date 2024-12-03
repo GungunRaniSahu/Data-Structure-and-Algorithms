8f04bd6e753f4d323cec91b8cf2e801212537730
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-03 14:34:37 +0530

    Create example3.py

---
+++ Python/Day 03/example3.py
@@ -0,0 +1,9 @@
+#Find the Largest Number in a List
+numbers = [3, 5, 7, 2, 8, 1]
+largest = numbers[0]
+
+for num in numbers:
+    if num > largest:
+        largest = num
+
+print(f"The largest number is {largest}.")
