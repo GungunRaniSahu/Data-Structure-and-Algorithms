3896eb3cb3c697d387bd6095dcf744aabadf2ae6
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-05 22:10:34 +0530

    Create remove.py

---
+++ Python/Day 05/remove.py
@@ -0,0 +1,8 @@
+my_list = [10, 20, 30, 40, 50]
+# Remove 40
+my_list.remove(40)
+print(my_list)  # Output: [10, 15, 25, 30, 50, 60]
+
+# Remove and print the last element
+print(my_list.pop())  # Output: 60
+print(my_list)  # Output: [10, 15, 25, 30, 50]
