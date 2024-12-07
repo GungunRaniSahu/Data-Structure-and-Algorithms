a58cd23cc00763f61a6449b54ac6985141a8100e
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-07 15:50:58 +0530

    Create dict_delete.py

---
+++ Python/Day 07/dict_delete.py
@@ -0,0 +1,6 @@
+my_dict = {"name": "Gungun", "age": 18}
+my_dict.pop("age")  # Removes 'age' key
+print(my_dict)  # Output: {'name': 'Gungun'}
+
+del my_dict["name"]  # Deletes 'name' key
+print(my_dict)  # Output: {}
