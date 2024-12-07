fc1d3d35139e36adaf739cb6090e6d96e0d3540f
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-07 15:49:19 +0530

    Create dict_access.py

---
+++ Python/Day 07/dict_access.py
@@ -0,0 +1,4 @@
+my_dict = {"name": "Gungun", "age": 20}
+print(my_dict["name"])  # Output: Gungun
+print(my_dict.get("age"))  # Output: 20
+print(my_dict.get("gender", "Not Specified"))  # Output: Not Specified
