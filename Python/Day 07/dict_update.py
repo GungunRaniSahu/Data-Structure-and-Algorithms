aba0d73341bf2d52f4a92a3deea4b6c034144975
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-07 15:50:05 +0530

    Create dict_update.py

---
+++ Python/Day 07/dict_update.py
@@ -0,0 +1,4 @@
+my_dict = {"name": "Gungun"}
+my_dict["age"] = 18  # Adding a new key-value pair
+my_dict["name"] = "Rani"  # Updating an existing key-value pair
+print(my_dict)  # Output: {'name': 'Rani', 'age': 18}
