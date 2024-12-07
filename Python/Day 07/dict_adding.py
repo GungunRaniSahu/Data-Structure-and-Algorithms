f5937bf64e3de5d8bacc6d4bb83ba7ec66536d71
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-07 15:53:01 +0530

    Create dict_adding.py

---
+++ Python/Day 07/dict_adding.py
@@ -0,0 +1,4 @@
+my_dict = {"name": "Gungun"}
+update_data = {"age": 18, "city": "Ranchi"}
+my_dict.update(update_data)  # Merges dictionaries
+print(my_dict)  # Output: {'name': 'Gungun', 'age': 18, 'city': 'Ranchi'}
