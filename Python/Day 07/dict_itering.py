a20974cd4dcd36402c2b4be82899f64bffabf256
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-07 15:51:43 +0530

    Create dict_itering.py

---
+++ Python/Day 07/dict_itering.py
@@ -0,0 +1,13 @@
+my_dict = {"name": "Gungun", "age": 18}
+
+# Iterating through keys
+for key in my_dict.keys():
+    print(key)  # Output: name, age
+
+# Iterating through values
+for value in my_dict.values():
+    print(value)  # Output: Gungun, 18
+
+# Iterating through key-value pairs
+for key, value in my_dict.items():
+    print(f"{key}: {value}")  # Output: name: Gungun, age: 18
