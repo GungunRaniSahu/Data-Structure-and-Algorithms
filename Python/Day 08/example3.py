3c0598822769946d3d2f1f9a5b513400d039feed
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-08 17:38:32 +0530

    Create example3.py

---
+++ Python/Day 08/example3.py
@@ -0,0 +1,5 @@
+#Symmetric Difference in Surveys
+survey1 = {"Python", "Java", "C++"}
+survey2 = {"Python", "JavaScript", "Ruby"}
+unique_choices = survey1.symmetric_difference(survey2)
+print(unique_choices)  # Output: {"C++", "JavaScript", "Ruby"}
