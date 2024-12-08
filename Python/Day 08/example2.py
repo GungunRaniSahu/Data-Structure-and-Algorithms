3edc4a7d8f09991b0ba6ca86c4430e0817c251dc
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-08 17:38:13 +0530

    Create example2.py

---
+++ Python/Day 08/example2.py
@@ -0,0 +1,5 @@
+#Finding Common Students in Two Classes
+class_a = {"Alice", "Bob", "Charlie"}
+class_b = {"Bob", "David", "Eve"}
+common_students = class_a.intersection(class_b)
+print(common_students)  # Output: {"Bob"}
