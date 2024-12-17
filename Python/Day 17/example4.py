e7dea411b5a29ca19c667ebb74b1653af672e313
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-17 22:28:50 +0530

    Feat : Fibonacci Series with Space Optimization

---
+++ Python/Day 17/example4.py
@@ -0,0 +1,11 @@
+#Fibonacci Series with Space Optimization
+def fibonacci_optimized(n):
+    if n <= 1:
+        return n
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+
+n = 10
+print([fibonacci_optimized(i) for i in range(n)])
