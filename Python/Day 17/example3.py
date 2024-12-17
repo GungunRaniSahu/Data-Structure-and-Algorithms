89b6ff6d3abcf3779672e4fee654212e44371f08
Gungun Rani <155472014+GungunRaniSahu@users.noreply.github.com>
2024-12-17 22:28:12 +0530

    Feat : Fibonacci Series with Tabulation

---
+++ Python/Day 17/example3.py
@@ -0,0 +1,12 @@
+#Fibonacci Series with Tabulation
+def fibonacci_tabulation(n):
+    if n <= 1:
+        return n
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+    return dp[n]
+
+n = 10
+print([fibonacci_tabulation(i) for i in range(n)])
